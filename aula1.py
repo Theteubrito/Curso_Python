becd1226404ed0d5b9d99b3d54903cecdd107d80
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-04 20:31:47 -0300

    aula um finalizada

---
+++ aula1.py
@@ -0,0 +1,17 @@
+# isso é um comentario
+
+print(123)
+
+"""
+Isso é uma DocString
+
+nao é um comentario, mas tambem é utilizado para comentarios.
+"""
+
+print (456)
+
+'''
+isso tambem é uma DocString
+porem com aspas simples.
+
+'''
