b61ad717e237f6bb0337c5b21d37416635c87a2f
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-05 17:13:20 -0300

    aula4 finalizada

---
+++ aula4.py
@@ -0,0 +1,31 @@
+# Tipagem int e float
+'''
+int -> Numero Inteiro
+O tipo int representa qualquer numero inteiro, positivo ou negativo
+(int sem sinal é considerado positivo)
+
+float -> Numero  com ponto flutuante
+O tipo float representa qualquer numero com ponto flutuante, positivo ou negativo
+(float sem sinal é considerado positivo)
+
+EX:
+'''
+print(11) # int
+print(-11) #int
+
+print(1.1) # float
+print(-1.1) # float
+
+# Type
+'''
+A função(na verdade é uma classe) type mostra o tipo q o pythom inferiu ao valor.
+
+ex:
+'''
+print(   type('Matheus')   )
+
+print(   type(11)   )
+print(   type(-11)   )
+
+print(   type(1.1)   )
+print(   type(-1.1)   )
