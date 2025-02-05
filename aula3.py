474be3eb31c26fb1bf91c77280a38062613b8534
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-05 16:55:47 -0300

    aula3 finalizada

---
+++ aula3.py
@@ -0,0 +1,29 @@
+'''
+Python = Linguagem de programação
+Tipo de tipagem = Dinamica/ForteS -> Ele identiifica 
+a informação q esta recebendo sem a necessidade de 
+definir manualmente o tipo.
+EX:
+'''
+print('Ola mundo')
+# ele identificou altomaticamente q o argumento recebido é do tipo str
+
+
+'''
+str -> Striing -> texto
+String são textos q estao dentro de aspas.
+'''
+
+#Aspas Simples
+print('Matheus')
+print('"Matheus"')
+
+#Aspas Duplas
+print("Matheus")
+print("'Matheus'")
+
+#Escape
+print("\"Matheus\"")
+
+#r
+print(r"\"Matheus\"")
