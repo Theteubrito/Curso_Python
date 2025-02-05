cc361e4aa84ac533ac00310758ba8bfef7c8d603
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-05 17:33:42 -0300

    aula 5 finalizada

---
+++ aula5.py
@@ -0,0 +1,24 @@
+# bool
+'''
+bool -> boolean (boleano)
+representa dois valores lógicos.
+Ao questionar algo em um programa, só existem duas resposta possíveis:
+-True -> Verdadeiro/sim
+ou
+-False - Falso/não
+
+Existem varios operadores para questionar. Dentre eles o == que é
+o operador logico que questiona se um valor é igual a outro.
+
+Ex:
+'''
+# o tipo
+print(  type(10==10)  )
+
+# questionamentos
+
+print(10==10) # 10 é igual a 10?
+print ('m'=='m') # a letra m é igual a letra m?
+
+print(10==11) # 10 é igual a 11?
+print(10==-10) # 10 é igual a menos 10?
