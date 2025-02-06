2bb2b1a205bf39410e3c5ce3ac74d34434923ebf
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-06 13:37:43 -0300

    aula 7 finalizada

---
+++ aula7.py
@@ -0,0 +1,31 @@
+'''
+Variaveis
+São usadas para salvar algo na memoria do computadoes.
+
+Regras da PEP8:
+inicie variaveis com letras minuculas.
+pode usar numeros e underline(escrita_em_snake_case).
+
+O operador de = é o operador de atribuição.
+Logo, é usado para atribuir um valor a um nome (variavel).
+
+Ex:
+'''
+nome_completo = 'Matheus Siqueira Vieira Brito'
+um_mai_um = 1+1
+
+print(nome_completo)
+print(um_mai_um)
+'''
+De acordo com o livro "Codigo limpo" é importante q o nome da variavel descreva 
+exatamente o valor q ela carrega.
+'''
+
+# outro exemplo:
+nome = 'matheus'
+idade = 29 #no dia 6/02/2025 as 13:30 eu tenho 29 anos
+maior_de_idade = idade >= 18 #"idade" é maior ou igual a 18
+
+print('Nome: ', nome)
+print('idade: ', idade)
+print('é maior de idade? ', maior_de_idade)
