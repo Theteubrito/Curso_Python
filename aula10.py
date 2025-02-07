1a58f1508026a9fd111b840a6b631cb9513806da
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-07 10:39:14 -0300

    aula 10 finalizada

---
+++ aula10.py
@@ -0,0 +1,15 @@
+# concatenação e repetição com operadores aritméticos
+
+# concatenação
+nome_compl = 'matheus' + ' '+ 'brito'
+print(nome_compl)
+
+
+
+# repetição
+d_vezes = 'a' * 10
+print(d_vezes)
+
+t_vezes = 3 * 'matheus_'
+print(t_vezes)
+
