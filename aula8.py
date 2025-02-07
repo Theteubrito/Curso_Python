0522b5b5edb9fc639ad540dbd868a1dfdbf960b6
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-06 21:05:48 -0300

    aula 8 finalizada

---
+++ aula8.py
@@ -0,0 +1,26 @@
+'''
+exercício:
+Crie as vareaveis exigidas pelo codigo abaixo com seus respectivos valores.
+
+print('nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_idade)
+print('Altura em metros: ', altura_metros)
+'''
+
+# resolução ¬
+nome = 'Matheus'
+sobrenome = "Brito"
+idade = 29
+ano_nascimento = 1995
+maior_idade = idade >=18
+altura_metros = 1.72
+
+print('Nome: ', nome)
+print('Sobrenome: ', sobrenome)
+print('Idade: ', idade)
+print('Ano de nascimento: ', ano_nascimento)
+print('É maior de idade? ', maior_idade)
+print('Altura em metros: ', altura_metros)
