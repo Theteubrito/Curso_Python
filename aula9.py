0ced8126e217bcb54bf2f87c5195fc6328a32d96
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-06 21:34:06 -0300

    aula 9 finalizada

---
+++ aula9.py
@@ -0,0 +1,44 @@
+# operações matemáticas
+
+adicao = 10 +10
+print('Adição:', adicao)
+
+subtracao = 10 - 5
+print('Subitração:', subtracao)
+
+multiplicacao = 10 * 10
+print('Multiplicação:', multiplicacao)
+
+divisao = 10 / 2.2 # sempre retornará um float
+print('Divisão:', divisao)
+
+divisao_inteira = 10 // 2.2
+print('Divisão inteira:', divisao_inteira)
+
+exponenciacao = 2**10
+print('Exponenciação:', exponenciacao)
+
+modulo = 55 % 2 # resto da divisão
+print('Modulo: ', modulo)
+
+
+# Exemplos de uso para o modulo
+
+
+# saber se um numero é multiplo de outro ¬
+
+dez_oito = 10 % 8 == 0 # o resto da divisão de 10 por 8 é igual a zero?
+print('10 é multipo de 8?:', dez_oito)
+
+deseseis_oito = 16 % 8 == 0 # o resto da divisão de 16 por 8 é igual a zero?
+print('16 é multipo de 8?:', deseseis_oito)
+
+
+
+# saber se um numero é impar ou par ¬
+
+impar = 5 % 2 == 0  # o resta da divisão de 5 para 2 é igual a zero?
+print('5 é impar?', impar)
+
+par = 10 % 2 == 0  # o resta da divisão de 10 para 2 é igual a zero?
+print('10 é par?', par)
