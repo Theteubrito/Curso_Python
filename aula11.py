5c95278f63f823a82569f43901d66b4628706e44
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-07 11:28:07 -0300

    aula 12 finalizada

---
+++ aula11.py
@@ -0,0 +1,10 @@
+# antecedencia dos operadores
+
+'''
+1 - (x+x)
+parenteces dentro de parenteses serão execultados primeiro
+2 - **
+3 - * / // %
+4 - + -
+em caso de prioridades iguais (2 + 1 - 1) a prioridade sera definida da esquerda para a direita
+'''
---
+++ aula12.py
@@ -0,0 +1,24 @@
+# Exercício
+'''
+nome =''
+altura = ...
+peso = ...
+imc =...
+
+saida ¬
+nome
+altura
+peso
+o seu imc é: ...
+'''
+
+# r ¬
+nome ='Matheus'
+altura = 1.72
+peso = 82.0
+imc = peso / (altura * altura)
+
+print('Nome:', nome)
+print('tem:', altura, 'de altura')
+print('Pesa:', peso)
+print('E o seu IMC é:', imc)
