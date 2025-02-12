e576f7087a664382d39aa4962689084b584bc8bc
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-11 21:53:45 -0300

    aula 18 finalizada

---
+++ aula18.py
@@ -0,0 +1,7 @@
+# O debug da aula 17
+'''
+Foi execultado um debug da aula 17 (oq criou uma pasta chamada .vscode com uma arquivo
+dentro chamado launcher.json).
+Isso foi feito para miostrar mais detalhadamento o fluxo de execução do codigo, facilitando
+a compreenção do condicionamento e a maneira q o codigo se comporta.
+'''
