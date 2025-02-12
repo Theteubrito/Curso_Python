c0942a817a3b30d81e009c36929d24475a31d47c
Matheus Brito <matheus-brito2011@hotmail.com>
2025-02-12 11:41:56 -0300

    aula 19 finalizada

---
+++ aula19.py
@@ -0,0 +1,32 @@
+# Operadores relacionais (comparação)
+'''
+OP -------Significado------- Exemplo (true)
+> ------------Maior--------- 2 > 1
+>= -----Maior ou igual------ 2 >= 2
+< -----------Menor---------- 1 < 2
+<= -----Menor ou igual------ 2 <= 2
+== ----------Igual---------- 'a' == 'a'
+!= --------Diferente-------- 'a' != 'b'
+
+
+Operadores  relacionais comparam dois valores retornado uma bool (true ou false)
+
+Ex:
+'''
+maior = 30 > 10
+maior_ou_igual = 10 >= 10
+menor = 20 < 100
+menor_ou_igual = 20 <= 20
+igual = 'oi' == 'oi'
+diferente = 'OI' != 'oi'
+
+'''
+pra execultar o exemplo acima de forma correta, você deve vizitar as variaveis atraves no pytthon 
+interativo no terminal.
+Pra fazer isso vc acessa a pasta onde esta a aula 19 dentro no terninal e usa o comando "python -i" seguido
+do nome deste arquivo (aula19.py).
+ou seja: "python -i aula19.py".
+
+Dentro do python interativo, é so digitar o nome da variavel e dar Enter para saber o retorno da mesma.
+Pra sair é so usar o comando "quit()".
+'''
